9868543b0f6f7ab5a9e0d44ca9668a1773b6c265
Victor <itsvicc@gmail.com>
2012-12-29 21:07:05 -0500

    Modified feedback form page to use ajax

--- php/members/feedback.php
+++ php/members/feedback.php
@@ -28,11 +28,21 @@ if (!checkLoggedIn()) {
         <script src="../../js/userConfig.js"></script>
         <script>
             function validateForm(){
-                if ($.trim($("#inputSuggestions").val()).length == 0){
-                    $("#inputSuggestions").next().show('slow');
+                var feedback = $("#inputSuggestions").val();
+                if ($.trim(feedback).length == 0){
+                    $("#success-info").slideUp();
+                    $("#error-info").slideDown();
                     return false;
                 }
-                return true;
+                $.ajax({
+                    type: "POST",
+                    url: "feedback_submit.php",
+                    data: {inputSuggestions: feedback}
+                }).done(function(msg) {
+                    $("#inputSuggestions").val('');
+                    $("#error-info").slideUp();
+                    $("#success-info").slideDown();
+                });
             }
 
             $(function(){
@@ -51,25 +61,28 @@ if (!checkLoggedIn()) {
                             <?php include("quickLinks.php"); ?>
                         </div>
                         <div class="span10">
+                            <div id="success-info" class="alert alert-success" style="display: none;">
+                                Success! Thanks for giving us a feedback. We will definitely consider it!
+                            </div>
+                            <div id="error-info" class="alert alert-error" style="display: none;">
+                                Feedback is empty...
+                            </div>
 							<div class="signup-box well">
-								<form class="form-horizontal" action="feedback_submit.php" method="post" onsubmit="return validateForm()">
-									<legend>Feedback</legend>
-                                    <div class="alert alert-info">
-                                        Let us know how we're doing! We're happy to take suggestions from our members to improve ACM. Remember, this is completely anonymous so say anything you want!
-                                    </div>
-									<div class="control-group">
-										<label class="control-label" for="inputSuggestions">Leave us a feedback!</label>
-										<div class="controls">
-											<textarea id="inputSuggestions" name="inputSuggestions"></textarea>
-											<span class="label label-important" style="display: none;">Please leave something!</span>
-										</div>
+								<legend>Feedback</legend>
+                                <div class="alert alert-info">
+                                    Let us know how we're doing! We're happy to take suggestions from our members to improve ACM. Remember, this is completely anonymous so say anything you want!
+                                </div>
+								<div class="control-group">
+									<label class="control-label" for="inputSuggestions">Leave us a feedback!</label>
+									<div class="controls">
+										<textarea id="inputSuggestions" name="inputSuggestions"></textarea>
 									</div>
-									<div class="control-group">
-										<div class="controls">
-											<button type="submit" class="btn btn-primary">Submit</button>
-										</div>
+								</div>
+								<div class="control-group">
+									<div class="controls">
+										<button onClick="validateForm();" class="btn btn-primary">Submit</button>
 									</div>
-								</form>
+								</div>
 							</div>
                         </div>
                     </div>
